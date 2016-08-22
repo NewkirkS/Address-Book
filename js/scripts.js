ad4ac86127d040fa62e2eee6790067013cdfe0d5
Caleb Stevenson and Stephen Newkirk <caleb@>
2016-08-22 09:32:00 -0700

    Create UI logic to display first and last names on submission.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
+// back end
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
 }
 var joe = new Contact("Joe");
 joe.lastName = "Schmoe";
-console.log(joe);
+
+$(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    $("ul#contacts").append("<li>" + newContact.firstName + "</li>");
+    $("ul#contacts").append("<li>" + newContact.lastName + "</li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
