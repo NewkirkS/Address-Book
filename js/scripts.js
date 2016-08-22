ac827f73e16887977d29b902b4277c3e24a269dc
Caleb Stevenson and Stephen Newkirk <caleb@>
2016-08-22 10:17:01 -0700

    Create prototype to concat first and last names in contacts section.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
 }
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
 
 // front end
 $(function() {
@@ -11,7 +14,7 @@ $(function() {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
     $(".contact").last().click(function(){
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName);
